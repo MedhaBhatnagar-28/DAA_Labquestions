846e94227c95b595e36dd1f94d68521ba6545b41
MedhaBhatnagar-28 <MEDHA.23682@stu.upes.ac.in>
2026-08-08 23:36:48 +0530

    LAB EXERCISE 2:  Write a program to count the frequency of each distinct element in an array.
    Assignment 1, Question2

---
+++ frequency.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, count;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int a[n], visited[n];
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        visited[i] = 0;
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(visited[i] == 1)
+            continue;
+
+        count = 1;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] == a[j])
+            {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d occurs %d times\n", a[i], count);
+    }
+
+    return 0;
+}
